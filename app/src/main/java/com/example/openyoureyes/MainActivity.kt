d940e403a48546a269d256a8e3d541043904e3ee
Danchil <lkhj0321@naver.com>
2023-09-15 18:31:15 +0900

    알람 리스트

--- app/src/main/java/com/example/openyoureyes/MainActivity.kt
+++ app/src/main/java/com/example/openyoureyes/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.openyoureyes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.openyoureyes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("hi")
     }
 
 }
